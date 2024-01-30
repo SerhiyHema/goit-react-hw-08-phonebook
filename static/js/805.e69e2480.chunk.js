ee84ab56116468f54d1aab83d546a9ee9f92e7b1
SerhiyHema <134592462+SerhiyHema@users.noreply.github.com>
2024-01-30 18:27:26 +0000

    Deploying to gh-pages from @ SerhiyHema/goit-react-hw-08-phonebook@87ea212bed45d5e415d598ded3f508ffce1a7f35 🚀

---
+++ static/js/805.e69e2480.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[805],{9805:function(e,o,n){n.r(o);var t=n(2791),c=n(184);o.default=function(){return(0,t.useEffect)((function(){setTimeout((function(){window.location="/goit-react-hw-08-phonebook"}),5e3)})),(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("h2",{children:"\u0421\u0442\u043e\u0440\u0456\u043d\u043a\u0430 \u043d\u0435 \u0437\u043d\u0430\u0439\u0434\u0435\u043d\u0430"})})}}}]);
+//# sourceMappingURL=805.e69e2480.chunk.js.map
